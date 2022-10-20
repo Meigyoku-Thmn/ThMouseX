90c9f1dcae112eaff6e1472b5ded20f435d50330
MeigyokuThmn <tranhuynhminhngoc1994@gmail.com>
2022-10-20 11:40:24 +0700

    add context menu for system tray icon

--- ThMouseX/main.cpp
+++ ThMouseX/main.cpp
@@ -11,6 +11,9 @@ namespace config = main::config;
 namespace messagequeuehook = core::messagequeuehook;
 
 constexpr auto MAX_LOADSTRING = 100;
+constexpr auto NOTIFICATION_ICON_MSG = WM_USER;
+constexpr auto SHOW_ABOUT_DIALOG = WM_USER + 1;
+constexpr auto EXIT_MSG = WM_USER + 2;
 
 HINSTANCE   hInst;							// current instance
 CHAR        szTitle[MAX_LOADSTRING];        // The title bar text
@@ -19,6 +22,8 @@ CHAR        szBalloonInfo[MAX_LOADSTRING];
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK DialogProc(HWND, UINT, WPARAM, LPARAM);
+void             ShowContextMenu(HWND hWnd);
+void             ShowAboutDialog(HWND hWnd, WPARAM wParam, LPARAM lParam);
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     UNREFERENCED_PARAMETER(hPrevInstance);
@@ -96,7 +101,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
                 .hWnd = hWnd,
                 .uID = 0,
                 .uFlags = NIF_ICON | NIF_MESSAGE | NIF_INFO | NIF_TIP,
-                .uCallbackMessage = WM_USER,
+                .uCallbackMessage = NOTIFICATION_ICON_MSG,
                 .hIcon = LoadIconA(hInst, MAKEINTRESOURCE(IDI_THMOUSEX)),
             };
             lstrcpyA(nid.szTip, szTitle);
@@ -105,13 +110,25 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
             Shell_NotifyIconA(NIM_ADD, &nid);
             return 0;
         }
-        case WM_USER:
+        case NOTIFICATION_ICON_MSG:
             switch (lParam) {
                 // double click on the system tray icon
                 case WM_LBUTTONDBLCLK:
-                    // if click exit then send close event
-                    if (DialogBoxA(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), NULL, DialogProc) == IDC_ExitButton)
-                        SendMessageA(hWnd, WM_CLOSE, wParam, lParam);
+                    ShowAboutDialog(hWnd, wParam, lParam);
+                    return 0;
+                case WM_RBUTTONDOWN:
+                case WM_CONTEXTMENU:
+                    ShowContextMenu(hWnd);
+                    return 0;
+            }
+            break;
+        case WM_COMMAND:
+            switch (LOWORD(wParam)) {
+                case SHOW_ABOUT_DIALOG:
+                    ShowAboutDialog(hWnd, wParam, lParam);
+                    return 0;
+                case EXIT_MSG:
+                    SendMessageA(hWnd, WM_CLOSE, wParam, lParam);
                     return 0;
             }
             break;
@@ -128,6 +145,24 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
         }
     }
     return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+void ShowContextMenu(HWND hWnd) {
+    POINT cursorPos;
+    GetCursorPos(&cursorPos);
+    auto hMenu = CreatePopupMenu();
+    if (hMenu) {
+        InsertMenuA(hMenu, -1, MF_BYPOSITION, SHOW_ABOUT_DIALOG, "&About");
+        InsertMenuA(hMenu, -1, MF_BYPOSITION, EXIT_MSG, "&Exit");
+        SetForegroundWindow(hWnd);
+        TrackPopupMenu(hMenu, TPM_BOTTOMALIGN, cursorPos.x, cursorPos.y, 0, hWnd, NULL);
+        DestroyMenu(hMenu);
+    }
+}
+
+void ShowAboutDialog(HWND hWnd, WPARAM wParam, LPARAM lParam) {
+    if (DialogBoxA(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), NULL, DialogProc) == IDC_ExitButton)
+        SendMessageA(hWnd, WM_CLOSE, wParam, lParam);
 }
 
 INT_PTR CALLBACK DialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
