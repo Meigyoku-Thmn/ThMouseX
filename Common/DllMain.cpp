54605fedc84abbb44a4f9f2c452a08b14f919e8c
Meigyoku Thmn <tranhuynhminhngoc1994@gmail.com>
2023-06-15 22:52:47 +0700

    avoid loading dll twice

--- Common/DllMain.cpp
+++ Common/DllMain.cpp
@@ -22,7 +22,9 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
             break;
         }
         case DLL_THREAD_ATTACH:
+            break;
         case DLL_THREAD_DETACH:
+            break;
         case DLL_PROCESS_DETACH:
             break;
     }
--- ThMCore/DllMain.cpp
+++ ThMCore/DllMain.cpp
@@ -27,9 +27,29 @@ using namespace std;
 WCHAR currentModuleDirPath[MAX_PATH + 1];
 WCHAR lastDirPath[MAX_PATH + 1];
 
+void LoadCurrentModuleDirPath() {
+    if (currentModuleDirPath[0] != '\0')
+        return;
+    HMODULE hmod;
+    auto dwFlags = GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT;
+    GetModuleHandleExW(dwFlags, (LPWSTR)&LoadCurrentModuleDirPath, &hmod);
+    GetModuleFileNameW(hmod, currentModuleDirPath, ARRAYSIZE(currentModuleDirPath));
+    currentModuleDirPath[ARRAYSIZE(currentModuleDirPath) - 1] = '\0';
+    PathRemoveFileSpecW(currentModuleDirPath);
+}
+
+HMODULE TryLoadInMemoryModule(const char* moduleName) {
+    auto modulePath = wstring(currentModuleDirPath) + L"\\" + wstring(moduleName, moduleName + strlen(moduleName));
+    return GetModuleHandleW(modulePath.c_str());
+}
+
 FARPROC WINAPI delayHook(unsigned dliNotify, PDelayLoadInfo pdli)
 {
+    LoadCurrentModuleDirPath();
     if (dliNotify == dliNotePreLoadLibrary) {
+        auto hMod = TryLoadInMemoryModule(pdli->szDll);
+        if (hMod != NULL)
+            return (FARPROC)hMod;
         GetCurrentDirectoryW(ARRAYSIZE(lastDirPath), lastDirPath);
         SetCurrentDirectoryW(currentModuleDirPath);
     }
@@ -40,21 +60,26 @@ FARPROC WINAPI delayHook(unsigned dliNotify, PDelayLoadInfo pdli)
     }
     return NULL;
 }
-ExternC const PfnDliHook __pfnDliNotifyHook2 = delayHook;
-ExternC const PfnDliHook __pfnDliFailureHook2 = delayHook;
+ExternC PfnDliHook __pfnDliNotifyHook2 = delayHook;
+ExternC PfnDliHook __pfnDliFailureHook2 = delayHook;
+
+void RemoveDelayHook() {
+    __pfnDliNotifyHook2 = NULL;
+    __pfnDliFailureHook2 = NULL;
+}
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call) {
         case DLL_PROCESS_ATTACH: {
+            RemoveDelayHook();
+
             setlocale(LC_ALL, ".UTF8");
             setlocale(LC_NUMERIC, "C");
 
             g_coreModule = hModule;
             g_targetModule = GetModuleHandleW(NULL);
 
-            GetModuleFileNameW(hModule, currentModuleDirPath, ARRAYSIZE(currentModuleDirPath));
-            currentModuleDirPath[ARRAYSIZE(currentModuleDirPath) - 1] = '\0';
-            PathRemoveFileSpecW(currentModuleDirPath);
+            LoadCurrentModuleDirPath();
 
             // We don't need thread notifications for what we're doing.
             // Thus, get rid of them, thereby eliminating some of the overhead of this DLL.
