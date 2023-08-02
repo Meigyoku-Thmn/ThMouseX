71e3cea733dbbbb2c721319105d1f3ee3fc098d3
MeigyokuThmn <tranhuynhminhngoc1994@gmail.com>
2023-08-02 10:32:31 +0700

    when ThMX inject early

--- ThMouseX/ConfigScripts/DFLYT.lua
+++ ThMouseX/ConfigScripts/DFLYT.lua
@@ -1,22 +1,47 @@
 -- LuaDllName = lua.dll
 -- Script to get player's position in Chaos of Black Loong
 -- This game was made in LuaSTGPlus
+
+local ori_metatb = getmetatable(_G)
 InitializeForLuaJIT();
-local ffi = require("ffi")
 
-local position = ffi.new('double[2]', {})
-SetPositionAddress(ffi.cast("DWORD", position))
+local function Setup()
+    local ffi = require("ffi")
 
-local ori_FrameFunc = FrameFunc
-FrameFunc = function()
-    local rs = ori_FrameFunc()
-    position[0] = player and player.x or 0
-    position[1] = player and player.y or 0
-    return rs
+    local position = ffi.new('double[2]', {})
+    SetPositionAddress(ffi.cast("DWORD", position))
+
+    local ori_FrameFunc = FrameFunc
+    FrameFunc = function()
+        local rs = ori_FrameFunc()
+        position[0] = player and player.x or 0
+        position[1] = player and player.y or 0
+        return rs
+    end
+
+    local onCloseCb = ffi.cast("UninitializeCallbackType", function(isProcessTerminating)
+        FrameFunc = ori_FrameFunc
+        onCloseCb:free()
+    end)
+    RegisterUninitializeCallback(onCloseCb)
 end
 
-local onCloseCb = ffi.cast("UninitializeCallbackType", function(isProcessTerminating)
-    FrameFunc = ori_FrameFunc
-    onCloseCb:free()
-end)
-RegisterUninitializeCallback(onCloseCb)
+if (FrameFunc ~= nil) then
+    Setup()
+else
+    local unpack = unpack or table.unpack
+    local metatb = {unpack (ori_metatb or {})}
+    function metatb:__newindex(name, value)
+        rawset(self, name, value)
+        if (name == "FrameFunc") then
+            setmetatable(_G, ori_metatb)
+            Setup()
+        end
+    end
+    setmetatable(_G, metatb)
+    local onCloseCb = ffi.cast("UninitializeCallbackType", function(isProcessTerminating)
+        setmetatable(_G, ori_metatb)
+        onCloseCb:free()
+    end)
+    RegisterUninitializeCallback(onCloseCb)
+end
