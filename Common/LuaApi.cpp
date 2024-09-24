456c67aabf414ce7216f01cb58f785aaef90b173
Meigyoku Thmn <tranhuynhminhngoc1994@gmail.com>
2024-09-25 00:30:03 +0700

    fix DLL export

--- Common/LuaApi.cpp
+++ Common/LuaApi.cpp
@@ -5,9 +5,8 @@
 #include "Log.h"
 #include "Variables.h"
 #include "CallbackStore.h"
-#include "MinHook.h"
 #include <MinHook.h>
-#include "../ThMouseX/resource.h"
+#include "PreparationScript.h"
 
 #include <Windows.h>
 #include <string>
@@ -18,7 +17,6 @@ namespace helper = common::helper;
 namespace memory = common::helper::memory;
 namespace encoding = common::helper::encoding;
 namespace callbackstore = common::callbackstore;
-namespace minhook = common::minhook;
 
 using namespace std;
 
@@ -74,4 +72,54 @@ namespace common::luaapi {
     MH_STATUS CreateHookApi(LPCSTR pszModule, LPCSTR pszProcName, LPVOID pDetour, LPVOID* ppOriginal) {
         return MH_CreateHookApi(encoding::ConvertToUtf16(pszModule).c_str(), pszProcName, pDetour, ppOriginal);
     }
+}
+
+namespace luaapi = common::luaapi;
+
+DWORD Lua_ReadUInt32(DWORD address) {
+    return luaapi::ReadUInt32(address);
+}
+
+DWORD Lua_ResolveAddress(DWORD* offsets, size_t length) {
+    return luaapi::ResolveAddress(offsets, length);
+}
+
+void Lua_OpenConsole() {
+    luaapi::OpenConsole();
+}
+
+void Lua_SetPositionAddress(DWORD address) {
+    luaapi::SetPositionAddress(address);
+}
+
+int Lua_GetDataType() {
+    return luaapi::GetDataType();
+}
+
+void Lua_RegisterUninitializeCallback(UninitializeCallbackType callback) {
+    luaapi::RegisterUninitializeCallback(callback);
+}
+
+int Lua_CreateHook(LPVOID pTarget, LPVOID pDetour, LPVOID* ppOriginal) {
+    return MH_CreateHook(pTarget, pDetour, ppOriginal);
+}
+
+int Lua_CreateHookApi(LPCSTR pszModule, LPCSTR pszProcName, LPVOID pDetour, LPVOID* ppOriginal) {
+    return luaapi::CreateHookApi(pszModule, pszProcName, pDetour, ppOriginal);
+}
+
+int Lua_EnableHook(LPVOID pTarget) {
+    return MH_EnableHook(pTarget);
+}
+
+int Lua_RemoveHook(LPVOID pTarget) {
+    return MH_RemoveHook(pTarget);
+}
+
+int Lua_DisableHook(LPVOID pTarget) {
+    return MH_DisableHook(pTarget);
+}
+
+LPCSTR Lua_StatusToString(int status) {
+    return MH_StatusToString((MH_STATUS)status);
 }
--- ThMouseX/CompilerConfig.cpp
+++ ThMouseX/CompilerConfig.cpp
@@ -1,4 +1,3 @@
-#include "../Common/PreparationScript.h"
 #pragma comment(lib, "shlwapi.lib")
 #pragma comment(lib, "psapi.lib")
 #pragma comment(lib, "DirectX8/Lib/D3dx8.lib")
