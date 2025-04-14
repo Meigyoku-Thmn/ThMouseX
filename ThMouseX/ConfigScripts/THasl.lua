421569e3e8f3adeb5cc2271ad2e03f4cfbdfc5c9
Meigyoku Thmn <tranhuynhminhngoc1994@gmail.com>
2025-04-14 22:16:25 +0700

    Centeryidong:FixedUpdate

--- ThMouseX/ConfigScripts/THasl.lua
+++ ThMouseX/ConfigScripts/THasl.lua
@@ -1,12 +1,12 @@
 -- Runtime = Unity Mono
 -- Script to get player's position in Abyss Soul Lotus
 local function CharacterUpdate(___transform: object): void
-    local position = ___transform.localPosition
+    local position = ___transform.position
     Position.X = position.x
     Position.Y = position.y
 end
 
 return {
-    --          Target method,           prehook,   posthook
-    {'Shooting.BaseCharacter:FixedUpdate', nil, CharacterUpdate},
+    --     Target method,      prehook,   posthook
+    {'Centeryidong:FixedUpdate', nil, CharacterUpdate},
 }
