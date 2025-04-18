6f8d12ca27d15e1523eec3ae6066687f7019b1c7
MeigyokuThmn <tranhuynhminhngoc1994@gmail.com>
2025-04-18 14:03:10 +0700

    update debug

--- ThMouseX/ConfigScripts/THasl.lua
+++ ThMouseX/ConfigScripts/THasl.lua
@@ -5,6 +5,8 @@ local VK_T = 0x54;
 local VK_Y = 0x59;
 
 local count = 59;
+local x = nil
+local y = nil
 
 local function CharacterUpdate(__instance: object): void
     local position = __instance.transform.position
@@ -13,15 +15,20 @@ local function CharacterUpdate(__instance: object): void
     
     position.z = 99999
 
+    if x ~= nil then position.x = x end
+    if y ~= nil then position.y = y end
+
     count = count + 1
     if count == 60 then
         count = 0        
         if IsKeyDown(VK_T) == true then
-            -- position.x = GetNumberInput();
+            -- x = GetNumberInput();
+            -- position.x = x
             print('VK_T')
         end
         if IsKeyDown(VK_Y) == true then
-            -- position.y = GetNumberInput();
+            -- y = GetNumberInput();
+            -- position.y = y
             print('VK_Y')
         end
         print("X = " .. position.x, "Y = " .. position.y)
